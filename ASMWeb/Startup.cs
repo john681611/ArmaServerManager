2406dbe089602501ce2e7274aad4b74aa3832b96
john681611 <john681611@hotmail.com>
2026-01-17 17:56:30 +0000

    Fix build error

--- ASMWeb/Startup.cs
+++ ASMWeb/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 
 namespace ASM.GUI.Web
 {
